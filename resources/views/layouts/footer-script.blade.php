ad8e2e20934a4ca86e2555358e841570dc6929da
Terc <robert.terc@gmail.com>
2021-08-25 01:26:31 -0400

    Vista Ready

---
+++ resources/views/layouts/footer-script.blade.php
@@ -0,0 +1,20 @@
+    <!--**********************************
+        Scripts
+    ***********************************-->
+    <!-- Required vendors -->
+    <script src="{{ asset('public/vendor/global/global.min.js')}}"></script>
+	<script src="{{ asset('public/vendor/bootstrap-select/dist/js/bootstrap-select.min.js')}}"></script>
+	<script src="{{ asset('public/vendor/chart.js/Chart.bundle.min.js')}}"></script>
+	
+	<!-- Chart piety plugin files -->
+    <script src="{{ asset('public/vendor/peity/jquery.peity.min.js')}}"></script>
+	
+	<!-- Apex Chart -->
+	<script src="{{ asset('public/vendor/apexchart/apexchart.js')}}"></script>
+	
+	<!-- Dashboard 1 -->
+	<script src="{{ asset('public/js/dashboard/dashboard-1.js')}}"></script>
+	
+	<script src="{{ asset('public/vendor/owl-carousel/owl.carousel.js')}}"></script>
+    <script src="{{ asset('public/js/custom.min.js')}}"></script>
+	<script src="{{ asset('public/js/deznav-init.js')}}"></script>
---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="keywords" content="" />
+	<meta name="author" content="Roberto Terc, Wilmy Rodriguez, Jairo Rojas" />
+	<meta name="robots" content="" />
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+	<meta name="description" content="Gestor de Proyectos Ver. 1" />
+	<meta name="format-detection" content="telephone=no">
+    <title>App Name </title>
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('public/images/favicon.png')}}">
+	<link rel="stylesheet" href="{{ asset('public/vendor/chartist/css/chartist.min.css')}}">
+    <link href="{{ asset('public/vendor/bootstrap-select/dist/css/bootstrap-select.min.css')}}" rel="stylesheet">
+	<link href="{{ asset('public/vendor/owl-carousel/owl.carousel.css')}}" rel="stylesheet">
+    <link href="{{ asset('public/css/style.css')}}" rel="stylesheet">
+	
+</head>
+
+<body>
+<h1>  TEst</h1>
+</body>
